1440d66a68391ef11cefe3320b86a2c802a2d830
Whethe <2529471940@qq.com>
2022-10-06 18:20:17 +0800

    change method testSubtract()

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -12,6 +12,6 @@ public class CalcTest {
     @Test
     void testSubtract(){
         Calc c = new Calc();
-        assertEquals(2, c.subtract(4,2));
+        assertEquals(6, c.subtract(4,2));
     }
 }
